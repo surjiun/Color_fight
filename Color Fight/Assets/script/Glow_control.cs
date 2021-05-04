dae6f9836001c8983205748d1999fd73a3d03b7f
sjn <dgg7734@naver.com>
2021-05-04 12:25:55 +0900

    숫자키로 색상 변경
    키패트 1,2번으로 칼이펙트와 캐릭터 색상 변경 가능

---
+++ Color Fight/Assets/script/Glow_control.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Glow_control : MonoBehaviour
+{
+    public Material d_material;
+    public Material c_material;
+    public GameObject sword;
+
+
+    private void Start()
+    {
+        d_material = GetComponent<SpriteRenderer>().material;
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            D();
+            
+           
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            Change();
+            
+        }
+    }
+    void Change()
+    {
+        GetComponent<SpriteRenderer>().material = c_material;
+        sword.GetComponent<SpriteRenderer>().material = c_material;
+    }
+
+    void D()
+    {
+        GetComponent<SpriteRenderer>().material = d_material;
+        sword.GetComponent<SpriteRenderer>().material = d_material;
+    }
+
+   
+
+}
+
+
+
